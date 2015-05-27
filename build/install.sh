bfd30e7bd4eda5e50d4131ba793aa9449e158598
majikina <majikina@users.noreply.github.com>
2015-05-27 11:11:11 +1000

    Update install.sh

--- build/install.sh
+++ build/install.sh
@@ -7,6 +7,7 @@ echo deb-src http://ppa.launchpad.net/bitcoin/bitcoin/ubuntu trusty main >> /etc
 apt-get -y update
 apt-get install -y bitcoind
 mkdir ~/.bitcoin
+echo "daemon=1\n" > ~/.bitcoin/bitcoin.conf
 echo "rpcuser=test\nrpcpassword=test\n" > ~/.bitcoin/bitcoin.conf
 echo "alias rt='bitcoind -regtest'" >> ~/.bashrc
 mkdir /etc/service/bitcoin
